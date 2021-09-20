e645a19fffbaa0061e7b1d6f6660fbb8122451db
krzys <krzysztofmuller1@gmail.com>
2021-09-20 23:43:55 +0200

    ćw_2

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* przykład 2 klasy */
-        /* najprostsze użycie klas */
-        List<Pojazd> pojazdList = new ArrayList<>();
-
-        Pojazd pojazd = new Pojazd();
-        pojazd.kolor = KolorEnum.NIEBIESKI;
-        pojazd.iloscKol = 4;
-        pojazd.nazwa = "Traktor";
-        pojazdList.add(pojazd);
-
-        pojazd = new Pojazd();
-        pojazd.iloscKol = 2;
-        pojazd.nazwa = "Rower";
-        pojazd.kolor = KolorEnum.CZERMONY;
-        pojazdList.add(pojazd);
-
-        for (Pojazd p : pojazdList) {
-            System.out.println(p.nazwa + " - " + p.iloscKol + " - " + p.kolor);
-        }
-
+        /* ćw 2 klasy */
+        /* zad.1
+            a) Stworzyć publiczną klasę Student z 2-ma polami String, 1 polem int i 1 polem boolean,
+            klasa ma się znajdować w osobnym pliku Student.java
+            b) stworzyć 3 objekty klasy student i wstawić je do tablicy
+            c) wyświetlić dane z tablicy, użyć pętli for
+         */
     }
-}
-
-/* Prosta klasa */
-class Pojazd {
-    String nazwa;
-    KolorEnum kolor;
-    int iloscKol;
-}
-
-enum KolorEnum {
-    CZERMONY, ZIELONY, NIEBIESKI
 }
