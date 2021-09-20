3783602aa93f957197bc27bc4325ff8d4fb8c991
krzys <krzysztofmuller1@gmail.com>
2021-09-20 22:42:06 +0200

    cw_1

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
         b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
         c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
-        d) niech w pętli są podawanę wartości liczbowe z klawiatury,
+        d) niech w pętli są podawane wartości liczbowe z klawiatury,
         e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
         f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
         do podanych cyfr przez użytkownika,
