efeb679c57000eb9dbda39b8f6d88a411ba02bc0
krzys <krzysztofmuller1@gmail.com>
2021-09-20 22:21:52 +0200

    przyklad_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Laboratorium 3");
+
+        /* lab1 enum-y */
+        /* Najprostsze Użycie enumów jako prosty typ wyliczeniowy */
+        System.out.println("kolor " + KolorEnum.CZERWONY);
+
+        /* użycie enuma */
+        String[] kolory = {KolorEnum.NIEBIESKI.toString(), KolorEnum.ZIELONY.toString()};
+        System.out.println("Drugi kolor to: " + kolory[1]);
+
+        /* Użycie enumów z booleanem i metodą */
+        drukujDoKonsoli(WielkoscEnum.MALY, "dlugopis");
+        drukujDoKonsoli(WielkoscEnum.DUZY, "Dom");
+    }
+
+    static void drukujDoKonsoli(WielkoscEnum wartosc, String przedmiot) {
+        System.out.println(przedmiot + " jest " + wartosc + " " + wartosc.wielkosc);
+    }
+}
+
+/* Prosty enum */
+enum KolorEnum {
+    CZERWONY, ZIELONY, NIEBIESKI;
+}
+
+/* enum z metodą i zmienną */
+enum WielkoscEnum {
+    MALY(false), DUZY(true), SREDNI(false), OGROMNY(true);
+
+    boolean wielkosc;
+
+    WielkoscEnum(boolean czyDuzy) {
+        wielkosc = czyDuzy;
     }
 }
