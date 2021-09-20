a9effc47251e28ed7b7826824ae7dc20ced8b53a
krzys <krzysztofmuller1@gmail.com>
2021-09-20 22:40:28 +0200

    cw_1

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        /* przykład 1 enum-y */
-        /* Najprostsze Użycie enumów jako prosty typ wyliczeniowy */
-        System.out.println("kolor " + KolorEnum.CZERWONY);
+        /* ćw 1 enum-y */
 
-        /* użycie enuma */
-        String[] kolory = {KolorEnum.NIEBIESKI.toString(), KolorEnum.ZIELONY.toString()};
-        System.out.println("Drugi kolor to: " + kolory[1]);
+        /* zad.1
+        a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
+        b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
+        c) stworzyć pętlę while z wartością true w warunku (nieskończona pętla),
+        d) niech w pętli są podawanę wartości liczbowe z klawiatury,
+        e) jeśli użytkownik poda z klawiatury 0 to program ma kończyć działanie,
+        f) umieścić w pętli switch() case: które będą wypisywały cyfry słownie odpowiednio
+        do podanych cyfr przez użytkownika,
+        g) gdy użytkownik wciśnie 0 ma wyświetlić się status KONIEC w innym wypadku KONTYNUUJEMY :),
+        */
 
-        /* Użycie enumów z booleanem i metodą */
-        drukujDoKonsoli(WielkoscEnum.MALY, "dlugopis");
-        drukujDoKonsoli(WielkoscEnum.DUZY, "Dom");
-    }
+        /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
+        * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
-    static void drukujDoKonsoli(WielkoscEnum wartosc, String przedmiot) {
-        System.out.println(przedmiot + " jest " + wartosc + " " + wartosc.wielkosc);
-    }
-}
-
-/* Prosty enum */
-enum KolorEnum {
-    CZERWONY, ZIELONY, NIEBIESKI;
-}
-
-/* enum z metodą i zmienną */
-enum WielkoscEnum {
-    MALY(false), DUZY(true), SREDNI(false), OGROMNY(true);
-
-    boolean wielkosc;
-
-    WielkoscEnum(boolean czyDuzy) {
-        wielkosc = czyDuzy;
     }
 }
