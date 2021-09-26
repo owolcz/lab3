bef0f3cc5d38a5f945c2c39f74b371d14307ed7c
krzys <krzysztofmuller1@gmail.com>
2021-09-26 10:17:15 +0200

    ćw_2

--- src/Punkt.java
+++ src/Punkt.java
@@ -1,59 +1,27 @@
 /* klasa Punkt */
 public class Punkt {
-    /* prywatne zmienne dostępne tylko w tym pliku */
-    private int x, y;
-    private char znak = 'y';
+    /* stworzyć zmienne prywatne pX, pY, pZ */
 
-    /* Pusty konstruktor */
-    Punkt() {
+    private int r;
+
+    @Override
+    public String toString() {
+        return "Punkt{" +
+                "r=" + r +
+                '}';
     }
 
-    /* konstruktor z parametrami */
-    Punkt(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+    /* stworzyć
+    a) pusty konstruktor,
+    b) konstruktor inicjalizujacy zmienną pX,
+    c) konstruktor inicjalizujący wszystkie zmienne,
+    d) stworzyć settery i gettery,
 
-    /* przeładowanie konstruktorów - wiele konstruktorów z inną liczbą parametrów lub różnymi typami*/
-    Punkt(int x, int y, int z) {
-        this.x = x;
-        this.y = y + z;
-    }
+        podpowiedz ALT + INSERT można wybrać generowanie automatyczne
+        konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
+    */
 
-    /* gettery - metody umożliwiający pośredni dostęp do zmiennych */
-    public int getX() {
-        return x;
+    public Punkt(int r) {
+        this.r = r;
     }
-
-    public int getY() {
-        return y;
-    }
-
-    /* settery - metody modyfikujące zmienne */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        zmienY(); // wywołanie prywatnej metody (dostęp do niej tylko w obrębie tego pliku i klasy)
-        this.y = y;
-    }
-
-    /* dowolne metody */
-    public int suma() {
-        return x + y;
-    }
-
-    public int suma(int z) {
-        return z + y;
-    }
-
-    public void wyswietl() {
-        System.out.println("Zmienna x: " + x + ", Zmienna y: " + y);
-    }
-
-    private void zmienY() { // prywatna metoda
-        System.out.println("Zmieniono zmienną " + znak);
-    }
-
 }
