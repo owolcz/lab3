fb092bff2c93177f61c398f495db85d5c116629b
krzys <krzysztofmuller1@gmail.com>
2021-09-26 10:17:51 +0200

    przykład 3

--- src/Punkt.java
+++ src/Punkt.java
@@ -2,15 +2,6 @@
 public class Punkt {
     /* stworzyć zmienne prywatne pX, pY, pZ */
 
-    private int r;
-
-    @Override
-    public String toString() {
-        return "Punkt{" +
-                "r=" + r +
-                '}';
-    }
-
     /* stworzyć
     a) pusty konstruktor,
     b) konstruktor inicjalizujacy zmienną pX,
@@ -20,8 +11,4 @@ public class Punkt {
         podpowiedz ALT + INSERT można wybrać generowanie automatyczne
         konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
     */
-
-    public Punkt(int r) {
-        this.r = r;
-    }
 }
