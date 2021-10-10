8a685519f837c84b510f344c46c5e13925271c34
krzys <krzysztofmuller1@gmail.com>
2021-10-10 12:28:58 +0200

    ćw 5

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        /* ćw 4 klasy */
+        /* ćw 5 Dziedziczenie */
 
-        /* zad.1
-            a) Stworzyć klasę Osoba z polami prywatnymi:
-            - imie (String),
-            - nazwisko (String),
-            - indeks (Integer)
-
-            b) stworzyc konstruktory, gettery i settery
-            c) napisać metodę w klasie Osoba która zwróci informację o osobie,
-         */
-
-        /* zad.2
-            a) Stworzyć klasę Student z polami prywatnymi
-            - Osoba osoba,
-            - WydzialEnum wydzial;
-            b) stworzyć enum WydzialEnum (w nowym pliku) z kilkoma polami,
-            c) stworzyc konstruktory, gettery i settery
-            d) nadpisać metodę toString aby zwracała wszystkie elementy ładnie przedstawione :)
-         */
-
-        /* zad.3
-            a) W klasie Main stworzyć 5 różnych studentów, (można za pomocą tego samego objektu)
-            b) dodać studentów do Listy ArrayList,
-            c) przeiterować listę za pomocą pętli forEach wywołując na każdym objekcie metodę toString() (w każdej iteracji);
+        /* zad.1 Proszę wybrać dowolny temat budowania struktur klas np. samochody, dom, biblioteka, nauka, zabawki, kosmetyki, biuro podróży ... .
+                 Będziemy budować podobną strukturę kodu jak w przykładzie 5,
+            a) Proszę stworzyć układ klas dziedziczących po sobie,
+            b) proszę użyć pól i metod protected jak i private,
+            c) proszę stworzyć konstruktory, metody: toString(), settery i gettery,
+            d) proszę używać super do wywołania konstruktorów i metod z klas nadrzędnych
+            (można wygenerować je --> prawy przycisk myszki --> generate --> settery, gettery, ...),
+            e) w klasie Main proszę stworzyć objekty podobnie jak w przykładzie 5,
+            oraz stworzyć listę typu najbardziej generycznego np. List<Budynek>
          */
 
     }
