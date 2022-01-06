ca8b5f8e26b3fa9368c7652bbd6e18b2cf48634d
oskar.wolcz <oskar.wolcz@infopro-insight.com>
2022-01-06 02:24:03 +0100

    Cw 1 - done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         /* ćw 1 enum-y */
-        //test
         /* zad.1
         a) Stworzyć prosty enum "LiczbyEnum" z liczbami od 1 do 6 zapisanymi słownie,
         b) stworzyć prosty enum "StatusEnum" z wartościami KONTYNUUJEMY i KONIEC,
@@ -20,5 +19,41 @@ public class Main {
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
+        while(true){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Podaj liczbę: ");
+            double first = scan.nextDouble();
+
+            String[] liczby = {LiczbyEnum.JEDEN.toString(), LiczbyEnum.DWA.toString(),LiczbyEnum.TRZY.toString(),
+                    LiczbyEnum.CZTERY.toString(),LiczbyEnum.PIĘĆ.toString(), LiczbyEnum.SZEŚĆ.toString()};
+
+            switch ((int) first) {
+                case 1 -> System.out.println(liczby[0]);
+                case 2 -> System.out.println(liczby[1]);
+                case 3 -> System.out.println(liczby[2]);
+                case 4 -> System.out.println(liczby[3]);
+                case 5 -> System.out.println(liczby[4]);
+                case 6 -> System.out.println(liczby[5]);
+            }
+
+            String[] status = {StatusEnum.KONTYNUJEMY.toString(), StatusEnum.KONIEC.toString()};
+            if (first == 0){
+                System.out.println(status[1]);
+                break;
+            }
+            else{
+                System.out.println(status[0]);
+            }
+        }
+
+
     }
+}
+
+enum LiczbyEnum{
+    JEDEN, DWA, TRZY, CZTERY, PIĘĆ, SZEŚĆ;
+}
+
+enum StatusEnum{
+    KONTYNUJEMY, KONIEC;
 }
