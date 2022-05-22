e8c5683c5857e1c85875a584365f049fc5252186
owolcz <wolczoskar@gmail.com>
2022-05-22 16:04:29 +0200

    Cw 2 - done

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        /* ćw 5 Dziedziczenie */
 
-        /* zad.1 Proszę wybrać dowolny temat budowania struktur klas np. samochody, dom, biblioteka, nauka, zabawki, kosmetyki, biuro podróży ... .
-                 Będziemy budować podobną strukturę kodu jak w przykładzie 5,
-            a) Proszę stworzyć układ klas dziedziczących po sobie,
-            b) proszę użyć pól i metod protected jak i private,
-            c) proszę stworzyć konstruktory, metody: toString(), settery i gettery,
-            d) proszę używać super do wywołania konstruktorów i metod z klas nadrzędnych
-            (można wygenerować je --> prawy przycisk myszki --> generate --> settery, gettery, ...),
-            e) w klasie Main proszę stworzyć objekty podobnie jak w przykładzie 5,
-            oraz stworzyć listę typu najbardziej generycznego np. List<Budynek>
-         */
 
         Pojazd pojazd = new Pojazd("Audi", "A4", 190);
         System.out.println(pojazd.toString());
